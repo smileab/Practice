4e4e4661d6fe0c0d30490cb8ccac0b1d3baab28e
smileab <1977606751@qq.com>
2017-12-21 14:25:42 +0800

    重构目录结构

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import App from '../src/js/App';
+import App from '../js/src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import Router from './src/js/common/Router';
+import Router from './js/src/common/Router';
 
 AppRegistry.registerComponent('AwesomeProject', () => Router);
